5057d685cd05868ca30a91ee66c943bc30e8ea5d
abad89 <abadalamenti89@gmail.com>
2021-07-16 04:32:03 -0400

    completed assignment

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,26 @@
+class Doctor
+    @@all = []
 
+    attr_accessor :name
+
+    def self.all
+        @@all
+    end
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
+    def appointments
+        Appointment.all.select {|appt| appt.doctor == self}
+    end
+
+    def patients
+        Appointment.all.map(&:patient)
+    end
+end
